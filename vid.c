eca2012bc6e6c7d07add7c5f5fd8e4ce84c5f866
Azizul Hakim <azizulfahim2002@gmail.com>
2015-03-27 03:26:02 -0400

    Remove some unused and unnecessary data structures

--- vid.c
+++ vid.c
@@ -37,25 +37,6 @@
 // A temp var just to see how many times hline_render is being called.
 int vline_count = 0;
 
-unsigned char sony_sdmhs53_edid[] = {0x00, 0xff, 0xff,0xff,
-					0xff,0xff,0xff,0x00,0x4d,0xd9,0x50,0x22,
-					0x01,0x01,0x01,0x01,0x0b,0x0e,0x01,0x03,
-					0x0c,0x1e,0x17,0x78,0xea,0x8c,0x3e,0xa4,
-					0x58,0x4d,0x91,0x24,0x15,0x4f,0x51,0xa1,
-					0x08,0x00,0x01,0x01,0x01,0x01,0x01,0x01,
-					0x01,0x01,0x01,0x01,0x01,0x01,0x01,0x01,
-					0x01,0x01,0x64,0x19,0x00,0x40,0x41,0x00,
-					0x26,0x30,0x18,0x88,0x36,0x00,0x30,0xe4,
-					0x10,0x00,0x00,0x18,0x00,0x00,0x00,0xfd,
-					0x00,0x39,0x3f,0x1c,0x31,0x09,0x00,0x0a,
-					0x20,0x20,0x20,0x20,0x20,0x20,0x00,0x00,
-					0x00,0xfc,0x00,0x53,0x44,0x4d,0x2d,0x48,
-					0x53,0x35,0x33,0x0a,0x20,0x20,0x20,0x20,
-					0x00,0x00,0x00,0xff,0x00,0x31,0x32,0x35,
-					0x33,0x37,0x30,0x36,0x0a,0x20,0x20,0x20,
-					0x20,0x20,0x00,0xce,
-};
-
 static struct fb_fix_screeninfo dlfb_fix = {
 	.id =           "udlfb",
 	.type =         FB_TYPE_PACKED_PIXELS,
@@ -113,9 +94,9 @@ static void dlfb_free_urb_list(struct beagleusb *dev);
 
 
 /* Function added by me to fix make errors */
-static void err (char *msg){
+/*static void err (char *msg){
 	printk(msg);
-}
+}*/
 
 /*
  * All DisplayLink bulk operations start with 0xAF, followed by specific code
@@ -421,7 +402,9 @@ static int dlfb_render_hline(struct beagleusb *dev, struct urb **urb_ptr,
 			      int *ident_ptr, int *sent_ptr)
 {				  
 	const u8 *line_start, *line_end, *next_pixel;
-	u32 dev_addr = dev->video.base16 + byte_offset;
+	int transferred = 0;
+	int retval;
+//	u32 dev_addr = dev->video.base16 + byte_offset;
 	
 	// For page y-index encoding
 	u8 *data;
@@ -436,9 +419,6 @@ static int dlfb_render_hline(struct beagleusb *dev, struct urb **urb_ptr,
 		*(data+1+1) = page_index >> 8;
 	}
 	
-	int transferred = 0;
-	int retval;
-
 	line_start = (u8 *) (front + byte_offset);
 	next_pixel = line_start;
 	line_end = next_pixel + byte_width;
@@ -459,7 +439,7 @@ static int dlfb_render_hline(struct beagleusb *dev, struct urb **urb_ptr,
 	      usb_sndbulkpipe(dev->usbdev, dev->bulk_out_endpointAddr),
 	      data, byte_width + 2 + 2, &transferred, HZ*5);
 		      
-	sent_ptr = transferred;
+	*sent_ptr = transferred;
 		      
 	printk("Return: %d transferred: %d \n", retval, transferred);
 	
@@ -472,7 +452,7 @@ static int dlfb_render_hline(struct beagleusb *dev, struct urb **urb_ptr,
 int dlfb_handle_damage(struct beagleusb *dev, int x, int y,
 	       int width, int height, char *data)
 {
-	int i, ret;
+	int i;
 	char *cmd;
 	cycles_t start_cycles, end_cycles;
 	int bytes_sent = 0;
@@ -1188,7 +1168,7 @@ static int dlfb_setup_modes(struct beagleusb *dev,
 			   struct fb_info *info,
 			   char *default_edid, size_t default_edid_size)
 {
-	int i, j;
+	int i;
 	const struct fb_videomode *default_vmode = NULL;
 	int result = 0;
 	char *edid = NULL;
@@ -1203,10 +1183,6 @@ static int dlfb_setup_modes(struct beagleusb *dev,
 	if (!edid) {
 		result = -ENOMEM;
 		goto error;
-	}
-	
-	for(j = 0; j < EDID_LENGTH; j++){
-		edid[j] = sony_sdmhs53_edid[j];
 	}
 
 	fb_destroy_modelist(&info->modelist);
